4be73c291d7b657ed6d23f9b5f01fbc1f77a7e2d
Victor Fragoso <victor.fragoso@mail.wvu.edu>
2016-09-30 09:44:37 -0400

    Skeleton for implementing different transformations

---
+++ transformations_labs.cc
@@ -0,0 +1,60 @@
+// Copyright (C) 2016 West Virginia University.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials provided
+//       with the distribution.
+//
+//     * Neither the name of West Virginia University nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Please contact the author of this library if you have any questions.
+// Author: Victor Fragoso (victor.fragoso@mail.wvu.edu)
+
+// This lab will be completed in class. The idea is to review all the
+// transformations material we covered in class and practice coding in C++.
+// The goal is to implement the functions declared below during class.
+// The functions are not well defined, that is, the signatures of the function
+// are not final. The second goal of the class is to choose the right signatures
+// for the functions that will compute the transformations.
+namespace {
+// This function will apply the translation transformation to a given input
+// point. Modify the signature to get the proper function.
+void Translate();
+
+// This function will apply the scaling transformation to a given input point.
+void Scale();
+
+// This function will apply a rotation transformation to a given input point.
+void Rotate();
+
+// This function builds a rotation matrix that rotates, scales and translates.
+void RotateScaleAndTranslate();
+}  // namespace
+
+int main(int argc, char** argv) {
+  // TODO(vfragoso): Implement examples testing the translation, scale, and
+  // rotation transformation implementations.
+  return 0;
+}
